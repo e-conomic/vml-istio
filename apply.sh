886365d942b5de476d975fd4ee9266584e465f8a
JacobSMoller <scherffenberg91@gmail.com>
2020-08-14 11:36:20 +0200

    Latest version of generated files

--- apply.sh
+++ apply.sh
@@ -1,4 +1,3 @@
-
 kubectl apply -f ../ssn-annotator/manifests/annotator.yaml
 kubectl -n ssn set image deploy/annotator annotator=eu.gcr.io/dev-vml-cm/ssn-annotator:master
 
@@ -6,7 +5,13 @@ kubectl apply -f ../ssn-dataservice/manifests/dataservice.yaml
 kubectl -n ssn set image deploy/dataservice dataservice=eu.gcr.io/dev-vml-cm/ssn-dataservice:master
 
 kubectl apply -f ../asgt-cserving/manifests/jester.yaml
-kubectl -n ssn set image deploy/jester jester=eu.gcr.io/dev-vml-cm/asgt-cserving:master
+kubectl -n asgt set image deploy/jester jester=eu.gcr.io/dev-vml-cm/asgt-cserving:master
 
 kubectl apply -f ../asgt-meta-data/manifests/meta-data.yaml
-kubectl -n ssn set image deploy/meta-data meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
+kubectl -n asgt set image deploy/meta-data meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
+
+kubectl apply -f ../asgt-meta-data/manifests/meta-data.yaml
+kubectl -n asgt set image deploy/meta-data meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
+kubectl -n asgt set image cronjob/asgt-delete-datasets-job meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
+kubectl -n asgt set image cronjob/asgt-delete-models-job meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
+kubectl -n asgt set image cronjob/asgt-train-models-job meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
