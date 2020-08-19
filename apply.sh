e62b5803bdbaac1b242adbf9b476ba2371de7ed5
JacobSMoller <scherffenberg91@gmail.com>
2020-08-19 11:59:16 +0200

    Add external service cluster instead of patching

--- apply.sh
+++ apply.sh
@@ -1,6 +1,9 @@
 kubectl apply -f ../ssn-annotator/manifests/annotator.yaml
 kubectl -n ssn set image deploy/annotator annotator=eu.gcr.io/dev-vml-cm/ssn-annotator:master
 
+kubectl apply -f ../ssn-envoy-services/manifests/envoy-services.yaml
+kubectl -n ssn set image deploy/envoy-services envoy-services=eu.gcr.io/dev-vml-cm/ssn-envoy-services:master
+
 kubectl apply -f ../ssn-dataservice/manifests/dataservice.yaml
 kubectl -n ssn set image deploy/dataservice dataservice=eu.gcr.io/dev-vml-cm/ssn-dataservice:master
 
@@ -9,8 +12,6 @@ kubectl -n asgt set image deploy/jester jester=eu.gcr.io/dev-vml-cm/asgt-cservin
 
 kubectl apply -f ../asgt-meta-data/manifests/meta-data.yaml
 kubectl -n asgt set image deploy/meta-data meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
-
-kubectl apply -f ../asgt-meta-data/manifests/meta-data.yaml
 kubectl -n asgt set image deploy/meta-data meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
 kubectl -n asgt set image cronjob/asgt-delete-datasets-job meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
 kubectl -n asgt set image cronjob/asgt-delete-models-job meta-data=eu.gcr.io/dev-vml-cm/asgt-meta-data:master
